d1f3587a60d7920431df8e10ff3b0b673187a0f4
Amitabh Kumar <amitabh.kumar2@gmail.com>
2020-01-26 15:11:56 -0500

    adding

--- ExpressClac.js
+++ ExpressClac.js
@@ -1,46 +1,45 @@
-var app = require('express') ();
+var app = require("express")();
 
-var bodyParser = require('body-parser');
+var bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
 
 PORT = process.env.PORT || 3000;
 
 app.get("/operations/:firstNum/:secondNum", function(req, res) {
-
   var operations = req.params.operations;
-  
+
   var firstNum = parseInt(req.params.firstNum);
   var secondNum = parseInt(req.params.secondNum);
   var result;
 
   switch (operations) {
-
     case "add":
     case "+":
       result = firstNum + secondNum;
-      
-      case "subtract":
-      case "-":
-        result = firstNum - secondNum;
-        break;
-        case "multiply":
-          case "*":
-            result = firstNum * secondNum;
-            break;
-            case "/":
-              result = firstNum / secondNum;
-              break;
-              default:
-              result = "Sorry! The only valid operations are add, subtract, multiply, and divide.";
 
+    case "subtract":
+    case "-":
+      result = firstNum - secondNum;
+      break;
+    case "multiply":
+    case "*":
+      c;
+      result = firstNum * secondNum;
+      break;
+    case "/":
+      result = firstNum / secondNum;
+      break;
+    default:
+      result =
+        "Sorry! The only valid operations are add, subtract, multiply, and divide.";
   }
 
+  console.log(result);
+
   req.send(result.toString());
-
-
 });
 
-app.listen(PORT, function () {
+app.listen(PORT, function() {
   console.log("Listening on " + PORT);
 });
